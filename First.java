01c287aa1e252b7644a9b6ea7116578ee061b54a
Akshay More <45824962+theakshaymore@users.noreply.github.com>
2023-03-08 12:13:36 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,44 @@
+
+public class First {
+
+    public static void main(String[] args) {
+
+        // Primitive
+        // int
+        int var1 = 5;
+        byte var2 = 127;
+        short sh = 33;
+        long l = 32323l;
+
+        // float
+        float f = 4.4f;
+        double d = 4.55443;
+
+        // character
+        char k = 'd';
+
+        // boolean
+        boolean res = true;
+
+        int num = 5;
+        int num2 = 4;
+        int result = num + num2;
+
+        byte a = 4;
+        System.out.println(a);
+
+        double marks = 5.43232323232;
+
+        float percentage = 3.4f;
+
+        System.out.println(percentage);
+
+        System.out.println(marks);
+
+        System.out.println(result);
+
+        char c = 'k';
+        System.out.println(c);
+
+    }
+}
