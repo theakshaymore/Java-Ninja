3da9715432a6557f42819d42e411a527beab1842
Akshay More <45824962+theakshaymore@users.noreply.github.com>
2023-03-08 12:31:01 +0530

    literals

---
+++ Literals.java
@@ -0,0 +1,18 @@
+public class Literals {
+    public static void main(String[] args) {
+        // literals
+
+        int num1 = 0b0101;
+        System.out.println(num1);
+
+        int num2 = 0x7E;
+        System.out.println(num2);
+
+        int num3 = 10_00_00_000;
+        System.out.println(num3);
+
+        char c = 'a';
+        // c = c + 1;
+        System.out.println(c + 1);
+    }
+}
--- Variables.java
+++ Variables.java
@@ -1,5 +1,5 @@
 
-public class First {
+public class Variables {
 
     public static void main(String[] args) {
 
